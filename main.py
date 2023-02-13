d720c6ae4b5e2d6e1747a94bef344f41defa387b
Kevins Gutmanis <kevins.gutmanis@clarity.cx>
2023-02-13 15:11:21 +0200

    DKG_test2

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def find_mismatch(text):
                 return i+1
             opening_brackets_stack.pop()
             if opening_brackets_stack:
-                  return opening_brackets_stack[-1].position
+                  return opening_brackets_stack[0].position
             return "Success"
 
 
