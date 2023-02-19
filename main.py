66bd8b8e83f5f2ebed1a5b1a9cecaf32b9348c48
Kevins Gutmanis <kevins.gutmanis@clarity.cx>
2023-02-19 14:32:26 +0200

    Test#5

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ def find_mismatch(text):
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
+            opening_brackets_stack.pop()
     if opening_brackets_stack:
-        return opening_brackets_stack[-1].position
+        return opening_brackets_stack[0].position
         opening_brackets_stack.pop()
     return "Success"
 
